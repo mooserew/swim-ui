d2a1138dee0c1e606da848bfe8e0186fec415eeb
Pedro <xxferreira24@gmail.com>
2024-06-25 20:58:04 +0100

    Size fix

--- src/app/safe-url.pipe.spec.ts
+++ src/app/safe-url.pipe.spec.ts
@@ -1,8 +1,15 @@
 import { SafeUrlPipe } from './safe-url.pipe';
+import { DomSanitizer } from '@angular/platform-browser';
 
 describe('SafeUrlPipe', () => {
+  let sanitizer: DomSanitizer;
+
+  beforeEach(() => {
+    sanitizer = jasmine.createSpyObj('DomSanitizer', ['bypassSecurityTrustUrl']) as unknown as DomSanitizer;
+  });
+
   it('create an instance', () => {
-    const pipe = new SafeUrlPipe();
+    const pipe = new SafeUrlPipe(sanitizer);
     expect(pipe).toBeTruthy();
   });
 });
