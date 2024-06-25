f4ff04fa1a5d2bb72b35311df418968f5084f82a
Pedro <xxferreira24@gmail.com>
2024-06-25 22:07:58 +0100

    fix post feed

--- src/app/services/PostService.ts
+++ src/app/services/PostService.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,38 @@ export class PostService {
   private feedUrl = 'https://swim-api-production-1a4b.up.railway.app/post/following';
   private profilePostsUrl = 'https://swim-api-production-1a4b.up.railway.app/Swim/post/get';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cookieService: CookieService) {}
 
   getPosts(): Observable<any> {
-    return this.http.get(this.feedUrl, { withCredentials: true });
+    const token = this.getTokenFromCookie();
+    if (!token) {
+      throw new Error('No token found in cookies');
+    }
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
+
+    return this.http.get(this.feedUrl, { headers, withCredentials: true });
   }
 
   getPostsByUserName(username: string): Observable<any> {
+    const token = this.getTokenFromCookie();
+    if (!token) {
+      throw new Error('No token found in cookies');
+    }
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
+
     const url = `${this.profilePostsUrl}/${username}`;
-    return this.http.get(url, { withCredentials: true });
+    return this.http.get(url, { headers, withCredentials: true });
+  }
+
+  private getTokenFromCookie(): string | null {
+    return this.cookieService.get('jwt');
   }
 }
