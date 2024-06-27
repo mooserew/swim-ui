d25dd62e2cb3362c35d0f059e8ec57d26c55a74c
pedro.ferreira <pedro.ferreira@cmas-systems.com>
2024-06-27 15:05:02 +0100

    trying to fix stats 2

--- src/app/user-stats/user-stats.component.ts
+++ src/app/user-stats/user-stats.component.ts
@@ -1,37 +1,85 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SpotifyAuthService } from '../services/spotify-auth.service';
+import { catchError, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
+@Component({
+  selector: 'app-user-stats',
+  templateUrl: './user-stats.component.html',
+  styleUrls: ['./user-stats.component.css']
 })
-export class SpotifyAuthService {
-  private apiUrl = 'https://swim-api-production-1a4b.up.railway.app';
+export class UserStatsComponent implements OnInit {
+  topTracks: any[] = [];
+  topArtists: any[] = [];
+  accessToken: string | null = null;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private spotifyAuthService: SpotifyAuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      const code = params['code'];
+      if (code) {
+        this.spotifyAuthService.handleCallback(code).subscribe(response => {
+          this.accessToken = response.accessToken;
+          if (this.accessToken) {
+            localStorage.setItem('spotify_access_token', this.accessToken);
+          }
+          const refreshToken = response.refreshToken;
+          if (refreshToken) {
+            localStorage.setItem('spotify_refresh_token', refreshToken);
+          }
+          this.router.navigate(['/stats']); // Redirect to /stats after storing tokens
+        }, error => {
+          console.error('Error handling callback', error);
+          this.login();
+        });
+      } else {
+        this.accessToken = localStorage.getItem('spotify_access_token');
+        const refreshToken = localStorage.getItem('spotify_refresh_token');
+        if (this.accessToken) {
+          this.getTopArtistsAndTracks(this.accessToken);
+        } else if (refreshToken) {
+          this.spotifyAuthService.refreshAccessToken(refreshToken).subscribe(response => {
+            this.accessToken = response.accessToken;
+            if (this.accessToken) {
+              localStorage.setItem('spotify_access_token', this.accessToken);
+              this.getTopArtistsAndTracks(this.accessToken);
+            }
+          }, error => {
+            console.error('Error refreshing access token', error);
+            this.login();
+          });
+        } else {
+          this.login();
+        }
+      }
+    });
+  }
+
+  getTopArtistsAndTracks(accessToken: string): void {
+    this.spotifyAuthService.getUserTopArtists(accessToken).pipe(
+      switchMap(data => {
+        this.topArtists = data.items;
+        return this.spotifyAuthService.getUserTopTracks(accessToken);
+      }),
+      catchError(error => {
+        console.error('Error fetching top artists or tracks', error);
+        this.login();
+        return of(null); // Return a null observable to handle the error gracefully
+      })
+    ).subscribe(data => {
+      if (data) {
+        this.topTracks = data.items;
+      }
+    });
+  }
 
   login(): void {
-    window.location.href = `${this.apiUrl}/spotify/login`;
-  }
-
-  handleCallback(code: string): Observable<any> {
-    const params = new HttpParams().set('code', code);
-    return this.http.get<any>(`${this.apiUrl}/callback`, { params });
-  }
-
-  getUserTopTracks(accessToken: string): Observable<any> {
-    const params = new HttpParams().set('accessToken', accessToken);
-    return this.http.get<any>(`${this.apiUrl}/user/top-tracks`, { params });
-  }
-
-  getUserTopArtists(accessToken: string): Observable<any> {
-    const params = new HttpParams().set('accessToken', accessToken);
-    return this.http.get<any>(`${this.apiUrl}/user/top-artists`, { params });
-  }
-
-  refreshAccessToken(refreshToken: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/spotify/refresh-token`, {
-      refreshToken,
-    });
+    this.spotifyAuthService.login();
   }
 }
